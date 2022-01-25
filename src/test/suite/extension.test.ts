dd624754c12d37dae837fbfcf310521bc1c7ec43
thisisthedave <thisisthedave@gmail.com>
2022-01-24 23:45:58 -0800

    findDefinition tests for named params, variants, variant options

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -20,12 +20,13 @@ async function showFracasDocument(
     return { document, editor };
 }
 
-suite("Extension Test Suite", () => {
-    vscode.window.showInformationMessage("Start all tests.");
+const testFixtureDir = (vscode.workspace.workspaceFolders || [])[0].uri.fsPath;
+const rewardFrc = path.join(testFixtureDir, 'reward.frc');
+const abilityFrc = path.join(testFixtureDir, 'ability.frc');
+const abilityActionDefinesFrc = path.join(testFixtureDir, 'ability-action-defines.frc');
 
-    const rewardFrc = path.join((vscode.workspace.workspaceFolders || [])[0].uri.fsPath, 'reward.frc');
-
-    const rewardCountTextRange = new vscode.Range(6, 9, 6, 16); // selection around "#:count"
+suite("Editor Lib Tests", () => {
+    vscode.window.showInformationMessage("Start editor lib tests.");
 
     test("getSelectedSymbol returns word under cursor", async () => {
         await showFracasDocument(rewardFrc, new vscode.Range(6, 9, 6, 9));
@@ -38,24 +39,64 @@ suite("Extension Test Suite", () => {
         const symbol = getSelectedSymbol();
         assert.strictEqual(symbol, "ount");
     });
+});
+
+suite("Find Definition Tests", () => {
+    vscode.window.showInformationMessage("Start findDefinition tests.");
+    
+    test("findDefinition resolves a named parameter", async () => {
+        const { document } = await showFracasDocument(abilityFrc);
+        const defs: FracasDefinition[] = await findDefinition(document, new vscode.Position(42, 38)); // cursor within "#:net-playback-mode"
+        assert.strictEqual(defs.length, 1, "single definition not found");
+        assert.strictEqual(defs[0].kind, FracasDefinitionKind.keyword, "type definition kind is not 'parameter'");
+        assert.strictEqual(defs[0].completionKind, vscode.CompletionItemKind.Keyword, "type completion kind is not 'Keyword'");
+        assert.strictEqual(defs[0].symbol, "net-playback-mode");
+        assert.strictEqual(defs[0].location.uri.fsPath, abilityActionDefinesFrc);
+        assert.deepStrictEqual(defs[0].location.range, new vscode.Range(26, 32, 26, 49), "location of named parameter is not correct");
+    });
 
     test("findDefinition resolves a field def", async () => {
         const { document } = await showFracasDocument(rewardFrc);
-        const defs: FracasDefinition[] = await findDefinition(document, rewardCountTextRange.start);
+        const defs: FracasDefinition[] = await findDefinition(document, new vscode.Position(6, 9)); // selection at "#:count");
         assert.strictEqual(defs.length, 1, "single definition not found");
-        assert.strictEqual(defs[0].kind, FracasDefinitionKind.field, "field definition kind is not 'field'");
-        assert.strictEqual(defs[0].completionKind, vscode.CompletionItemKind.Field, "field completion kind is not 'field'");
+        assert.strictEqual(defs[0].kind, FracasDefinitionKind.keyword, "field definition kind is not 'field'");
+        assert.strictEqual(defs[0].completionKind, vscode.CompletionItemKind.Keyword, "field completion kind is not 'Keyword'");
         assert.strictEqual(defs[0].symbol, "count");
+        assert.strictEqual(defs[0].location.uri.fsPath, rewardFrc);
         assert.deepStrictEqual(defs[0].location.range, new vscode.Range(5, 9, 5, 14), "location of definition is not correct");
     });
 
     test("findDefinition resolves a type def", async () => {
-        const { document } = await showFracasDocument(rewardFrc); // cursor within "(range-int: ..."
-        const defs: FracasDefinition[] = await findDefinition(document, new vscode.Position(6, 21));
+        const { document } = await showFracasDocument(rewardFrc);
+        const defs: FracasDefinition[] = await findDefinition(document, new vscode.Position(6, 21)); // cursor within "(range-int: ..."
         assert.strictEqual(defs.length, 1, "single definition not found");
         assert.strictEqual(defs[0].kind, FracasDefinitionKind.type, "type definition kind is not 'type'");
         assert.strictEqual(defs[0].completionKind, vscode.CompletionItemKind.Struct, "type completion kind is not 'Struct'");
         assert.strictEqual(defs[0].symbol, "range-int");
+        assert.strictEqual(defs[0].location.uri.fsPath, rewardFrc);
         assert.deepStrictEqual(defs[0].location.range, new vscode.Range(1, 13, 1, 22), "location of definition is not correct");
     });
+
+    test("findDefinition resolves a variant", async () => {
+        const { document } = await showFracasDocument(abilityFrc);
+        const defs: FracasDefinition[] = await findDefinition(document, new vscode.Position(4, 62)); // cursor within "action-block"
+        assert.strictEqual(defs.length, 1, "single definition not found");
+        assert.strictEqual(defs[0].kind, FracasDefinitionKind.variant, "type definition kind is not 'variant'");
+        assert.strictEqual(defs[0].completionKind, vscode.CompletionItemKind.Struct, "type completion kind is not 'Struct'");
+        assert.strictEqual(defs[0].symbol, "action-block");
+        assert.strictEqual(defs[0].location.uri.fsPath, abilityActionDefinesFrc);
+        assert.deepStrictEqual(defs[0].location.range, new vscode.Range(3, 16, 3, 28), "location of definition is not correct");
+    });
+
+    test("findDefinition resolves a variant option", async () => {
+        const { document } = await showFracasDocument(abilityFrc);
+        const defs: FracasDefinition[] = await findDefinition(document, new vscode.Position(4, 12)); // cursor within "action-block-targeted:"
+        assert.strictEqual(defs.length, 1, "single definition not found");
+        assert.strictEqual(defs[0].kind, FracasDefinitionKind.variantOption, "type definition kind is not 'variantOption'");
+        assert.strictEqual(defs[0].completionKind, vscode.CompletionItemKind.Struct, "type completion kind is not 'Struct'");
+        assert.strictEqual(defs[0].symbol, "action-block-targeted");
+        assert.strictEqual(defs[0].location.uri.fsPath, abilityActionDefinesFrc);
+        assert.deepStrictEqual(defs[0].location.range, new vscode.Range(5, 3, 5, 11), "location of definition is not correct");
+    });
+
 });
